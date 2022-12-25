ef7ae5491358356c8479a755c09f4c2f657738c0
eashirokovaa <eashirokovaa@gmail.com>
2022-12-25 15:39:05 +0300

    Update

--- script.js
+++ script.js
@@ -1,21 +1,66 @@
-import Chart from 'chart.js/auto';
-const ctx = document.getElementById('myChart');
-    
-  let myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-          datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 1
-          }]
+const ctx = document.getElementById("myChart");
+const gradient = ctx.getContext("2d").createLinearGradient(0, 0, 0, 270);
+gradient.addColorStop(0, "rgba(203,0,235,1)");
+gradient.addColorStop(1, "rgba(203,0,235,0)");
+
+let myChart = new Chart(ctx, {
+  type: "line",
+  data: {
+    labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    datasets: [
+      {
+        label: "Daily tasks",
+        data: [5, 7, 8, 4, 6, 5, 2],
+        fill: true,
+        borderColor: "rgb(203, 0, 235)",
+        backgroundColor: gradient,
+        tension: 0.4,
+        borderWidth: 3,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      x: {
+        grid: {
+          display: false,
         },
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true
-            }
-          }
-        }
-      });
+      },
+      y: {
+        beginAtZero: true,
+        max: 9,
+        grid: {
+          display: false,
+        },
+      },
+    },
+    plugins: {
+      title: {
+        display: true,
+        text: "Weekly Activity",
+        align: "start",
+        font: {
+          size: 18,
+        },
+      },
+    },
+  },
+});
+const ctxx = document.getElementById("myPieChart");
+let myPieChart = new Chart(ctxx, {
+  type: "doughnut",
+  data: {
+    labels: ["Work", "Home", "Lifestyle"],
+    datasets: [
+      {
+        data: [18, 5, 7],
+        backgroundColor: [
+          "rgb(103, 59, 235)",
+          "rgb(138, 227, 168)",
+          "rgb(255, 205, 86)",
+        ],
+        hoverOffset: 4,
+      },
+    ],
+  },
+});
